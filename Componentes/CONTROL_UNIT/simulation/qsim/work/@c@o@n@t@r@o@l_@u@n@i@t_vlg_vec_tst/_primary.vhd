56c9beced86b29906e03cc76adb3fab10b583ad3
Gabriel Sanches Pinto <sanches.pinto@unesp.br>
2025-07-01 09:56:35 -0300

    Arquivando versão antiga e preparando pasta para recomeçar o projeto

--- Componentes/CONTROL_UNIT/simulation/qsim/work/@c@o@n@t@r@o@l_@u@n@i@t_vlg_vec_tst/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity CONTROL_UNIT_vlg_vec_tst is
-end CONTROL_UNIT_vlg_vec_tst;
--- Componentes/INST_MEM/simulation/qsim/work/@i@n@s@t_@m@e@m_vlg_sample_tst/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity INST_MEM_vlg_sample_tst is
-    port(
-        A               : in     vl_logic_vector(31 downto 0);
-        sampler_tx      : out    vl_logic
-    );
-end INST_MEM_vlg_sample_tst;
