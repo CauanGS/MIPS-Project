cb09aee28455b22e51229f1dfb8a13771cba7e3d
CauanGS <cauan.souza@unesp.br>
2025-07-04 19:52:51 -0300

    Pinagem do PC feita

--- src/DISPLAY_SETE_SEGMENTOS.vhd
+++ src/DISPLAY_SETE_SEGMENTOS.vhd
@@ -1,62 +1,36 @@
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
+use IEEE.STD_LOGIC_1164.all;
 
 entity DISPLAY_SETE_SEGMENTOS is
-    Port (
-        RESULTADO : in std_logic_vector(31 downto 0);
-        HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, HEX6, HEX7 : out std_logic_vector(6 downto 0)
+    port(
+        ENTRADA_EM_HEXA  : in  STD_LOGIC_VECTOR(3 downto 0);
+        SEG_SAIDA : out STD_LOGIC_VECTOR(6 downto 0) -- SEG_SAIDA(6 downto 0) -> gfedcba
     );
-end DISPLAY_SETE_SEGMENTOS;
+end entity;
 
 architecture behave of DISPLAY_SETE_SEGMENTOS is
-
-    signal VALOR : unsigned(31 downto 0);
-    type VETOR_DIGITOS is array(7 downto 0) of integer range 0 to 9;
-    signal DIGITOS : VETOR_DIGITOS;
-
-    -- Converte um número decimal de 0 a 9 para o padrão 7 segmentos
-    function DECODIFICAR(D : integer) return std_logic_vector is
-        variable SEGMENTO : std_logic_vector(6 downto 0);
-    begin
-        case D is
-            when 0 => SEGMENTO := "1000000";
-            when 1 => SEGMENTO := "1111001";
-            when 2 => SEGMENTO := "0100100";
-            when 3 => SEGMENTO := "0110000";
-            when 4 => SEGMENTO := "0011001";
-            when 5 => SEGMENTO := "0010010";
-            when 6 => SEGMENTO := "0000010";
-            when 7 => SEGMENTO := "1111000";
-            when 8 => SEGMENTO := "0000000";
-            when 9 => SEGMENTO := "0010000";
-            when others => SEGMENTO := "0000000";
-        end case;
-        return SEGMENTO;
-    end function;
-
 begin
-
-    VALOR <= unsigned(RESULTADO);
-
-    process(VALOR)
-        variable TEMPORARIO : unsigned(31 downto 0);
+    process(ENTRADA_EM_HEXA) --process para poder usar o case
     begin
-        TEMPORARIO := VALOR;
-        for i in 0 to 7 loop
-            DIGITOS(i) <= to_integer(TEMPORARIO mod 10);
-            TEMPORARIO := TEMPORARIO / 10;
-        end loop;
+        case ENTRADA_EM_HEXA is
+            -- Padrões ativos em baixo (0=ON, 1=OFF) para gfedcba
+            when "0000" => SEG_SAIDA <= "1000000"; -- 0
+            when "0001" => SEG_SAIDA <= "1111001"; -- 1
+            when "0010" => SEG_SAIDA <= "0100100"; -- 2
+            when "0011" => SEG_SAIDA <= "0110000"; -- 3
+            when "0100" => SEG_SAIDA <= "0011001"; -- 4
+            when "0101" => SEG_SAIDA <= "0010010"; -- 5
+            when "0110" => SEG_SAIDA <= "0000010"; -- 6
+            when "0111" => SEG_SAIDA <= "1111000"; -- 7
+            when "1000" => SEG_SAIDA <= "0000000"; -- 8
+            when "1001" => SEG_SAIDA <= "0010000"; -- 9
+            when "1010" => SEG_SAIDA <= "0001000"; -- A
+            when "1011" => SEG_SAIDA <= "0000011"; -- b
+            when "1100" => SEG_SAIDA <= "1000110"; -- C
+            when "1101" => SEG_SAIDA <= "0100001"; -- d
+            when "1110" => SEG_SAIDA <= "0000110"; -- E
+            when "1111" => SEG_SAIDA <= "0001110"; -- F
+            when others => SEG_SAIDA <= "1111111"; -- Desligado
+        end case;
     end process;
-
-    -- HEX0 mostra unidade, HEX7 mostra milhões
-    HEX0 <= DECODIFICAR(DIGITOS(0));
-    HEX1 <= DECODIFICAR(DIGITOS(1));
-    HEX2 <= DECODIFICAR(DIGITOS(2));
-    HEX3 <= DECODIFICAR(DIGITOS(3));
-    HEX4 <= DECODIFICAR(DIGITOS(4));
-    HEX5 <= DECODIFICAR(DIGITOS(5));
-    HEX6 <= DECODIFICAR(DIGITOS(6));
-    HEX7 <= DECODIFICAR(DIGITOS(7));
-
-end behave;
+end architecture;
