81858fcc9620ae74b707b72552c3ae2043423c97
CauanGS <cauan.souza@unesp.br>
2025-07-04 14:57:24 -0300

    Blocos decodificador princiapal e decodificardor ULA unidos em um novo bloco UNIDADE DE CONTROLE

--- simulation/qsim/processador_mips.do
+++ simulation/qsim/processador_mips.do
@@ -1,10 +1,10 @@
-onerror {exit -code 1}
+onerror {quit -f}
 vlib work
 vlog -work work processador_mips.vo
-vlog -work work Waveform2.vwf.vt
-vsim -novopt -c -t 1ps -L cycloneive_ver -L altera_ver -L altera_mf_ver -L 220model_ver -L sgate work.processador_mips_vlg_vec_tst -voptargs="+acc"
+vlog -work work processador_mips.vt
+vsim -novopt -c -t 1ps -L cycloneive_ver -L altera_ver -L altera_mf_ver -L 220model_ver -L sgate work.processador_mips_vlg_vec_tst
 vcd file -direction processador_mips.msim.vcd
 vcd add -internal processador_mips_vlg_vec_tst/*
 vcd add -internal processador_mips_vlg_vec_tst/i1/*
+add wave /*
 run -all
-quit -f
--- simulation/qsim/work/processador_mips/_primary.vhd
+++ simulation/qsim/work/processador_mips/_primary.vhd
@@ -2,20 +2,16 @@ library verilog;
 use verilog.vl_types.all;
 entity processador_mips is
     port(
-        BranchResult    : out    vl_logic;
+        HEX0            : out    vl_logic_vector(6 downto 0);
         CLK             : in     vl_logic;
-        RESET           : in     vl_logic;
-        BranchSinal     : out    vl_logic;
-        JumpSinal       : out    vl_logic;
-        ENTRADA_SHIFT_LEFT: out    vl_logic_vector(31 downto 0);
-        ENTRADA_ULA1    : out    vl_logic_vector(31 downto 0);
-        ENTRADA_ULA2    : out    vl_logic_vector(31 downto 0);
-        INSTR           : out    vl_logic_vector(31 downto 0);
+        HEX1            : out    vl_logic_vector(6 downto 0);
+        HEX2            : out    vl_logic_vector(6 downto 0);
+        HEX3            : out    vl_logic_vector(6 downto 0);
+        HEX4            : out    vl_logic_vector(6 downto 0);
+        HEX5            : out    vl_logic_vector(6 downto 0);
+        HEX6            : out    vl_logic_vector(6 downto 0);
+        HEX7            : out    vl_logic_vector(6 downto 0);
         PC              : out    vl_logic_vector(31 downto 0);
-        RD1             : out    vl_logic_vector(31 downto 0);
-        RD2             : out    vl_logic_vector(31 downto 0);
-        RESULTADO       : out    vl_logic_vector(31 downto 0);
-        SAIDA_EXTENSOR  : out    vl_logic_vector(31 downto 0);
-        SAIDA_SHIFT2L   : out    vl_logic_vector(31 downto 0)
+        RESULTADO       : out    vl_logic_vector(31 downto 0)
     );
 end processador_mips;
