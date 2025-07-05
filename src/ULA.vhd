33e3b08b686d28435436913b0a6347ab26be018e
Gabriel Sanches Pinto <sanches.pinto@unesp.br>
2025-07-05 17:22:02 -0300

    ULA Comentada

--- src/ULA.vhd
+++ src/ULA.vhd
@@ -13,11 +13,15 @@ entity ULA is
 end ULA;
 
 architecture behave of ULA is
-    signal INTEIRO_A, INTEIRO_B : signed(31 downto 0);  -- Mantemos como signed para lidar com sinais
-    signal RESULTADO_TEMPORARIO  : signed(31 downto 0);   -- Resultado também é signed
+
+	 -- Define os sinais INTEIRO_A e INTEIRO_B para serem usados como intermediários
+	 -- Sinais internos para realizar a aritmética com sinal
+    signal INTEIRO_A, INTEIRO_B : signed(31 downto 0); 
+    signal RESULTADO_TEMPORARIO  : signed(31 downto 0); 
+	 -- Estes sinais armazenarão a versão 'signed' das entradas.
 begin
 
-    -- Convertendo as entradas de std_logic_vector para signed
+    -- Convertendo do tipo STD_LOGIC_VECTOR para o tipo 'signed' para fazer as operações
     INTEIRO_A <= signed(ENTRADA_A);
     INTEIRO_B <= signed(ENTRADA_B);
 
@@ -25,33 +29,27 @@ begin
     variable TEMPORARIO : signed(31 downto 0);
 begin
     case OPERACAO is
-        when "000" =>
-            TEMPORARIO := INTEIRO_A and INTEIRO_B;
-        when "001" =>
-            TEMPORARIO := INTEIRO_A or INTEIRO_B;
-        when "010" =>
-            TEMPORARIO := INTEIRO_A + INTEIRO_B;
-        when "100" =>
-            TEMPORARIO := INTEIRO_A and (not INTEIRO_B);
-        when "101" =>
-            TEMPORARIO := INTEIRO_A or (not INTEIRO_B);
-        when "110" =>
-            TEMPORARIO := INTEIRO_A - INTEIRO_B;
-        when "111" =>
+        when "000" => TEMPORARIO := INTEIRO_A and INTEIRO_B; -- Operação AND bit a bit
+        when "001" => TEMPORARIO := INTEIRO_A or INTEIRO_B; -- Operação OR bit a bit
+        when "010" => TEMPORARIO := INTEIRO_A + INTEIRO_B; -- Operação SOMA (ADD) com sinal
+        when "100" => TEMPORARIO := INTEIRO_A and (not INTEIRO_B); -- Operação A AND (NOT B)
+        when "101" => TEMPORARIO := INTEIRO_A or (not INTEIRO_B); -- Operação A OR (NOT B)
+        when "110" => TEMPORARIO := INTEIRO_A - INTEIRO_B; -- Operação SUBTRAÇÃO (SUB) com sinal
+        when "111" => -- Operação SET ON LESS THAN (SLT) com sinal
             if INTEIRO_A < INTEIRO_B then
-                TEMPORARIO := (others => '0');
-                TEMPORARIO(0) := '1';
+                TEMPORARIO := (others => '0'); -- zera o vetor
+                TEMPORARIO(0) := '1'; -- seta o bit menos significativo pra 1
             else
-                TEMPORARIO := (others => '0');
+                TEMPORARIO := (others => '0'); 
             end if;
         when others =>
-            TEMPORARIO := (others => '0');
+            TEMPORARIO := (others => '0'); -- Resultado padrão é 0 para operações não definidas
     end case;
-    RESULTADO_TEMPORARIO <= TEMPORARIO;
+    RESULTADO_TEMPORARIO <= TEMPORARIO; -- resultado temporário recebe resultado para que possa sair do processo
 end process;
-    -- Atribuindo o resultado
+    -- Converte o resultado interno (signed) de volta para STD_LOGIC_VECTOR
     RESULTADO <= std_logic_vector(RESULTADO_TEMPORARIO);
 
-    -- Sinalizando Zero, se o resultado for zero
+    -- sinalizador ZERO recebe '1' se o resultado for zero, '0' caso contrário
     ZERO <= '1' when RESULTADO_TEMPORARIO = 0 else '0';
 end behave;
