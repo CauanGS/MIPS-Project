71ffdf205b307297539fdfad6bc0f4e3a0e974ed
Gabriel Sanches Pinto <sanches.pinto@unesp.br>
2025-07-05 16:45:15 -0300

    Unidade de controle comentada

--- src/ULA_DECODIFICADOR.vhd
+++ src/ULA_DECODIFICADOR.vhd
@@ -9,18 +9,18 @@ end;
 
 architecture behave of ULA_DECODIFICADOR is 
 begin 
-	process(all)
+	process(all) 
 	begin 
 		case OPERACAO_ULA is
-		when "00" => CONTROLE_ULA <= "010"; -- add (for lw/sw/addi)
-		when "01" => CONTROLE_ULA <= "110"; -- sub (for beq)
-		when others => case CODIGO_FUNCAO is -- R-type instructions
-			when "100000" => CONTROLE_ULA <= "010"; -- add
-			when "100010" => CONTROLE_ULA <= "110";-- sub
-			when "100100" => CONTROLE_ULA <= "000"; -- and
-			when "100101" => CONTROLE_ULA <= "001"; -- or
-			when "101010" => CONTROLE_ULA <= "111"; -- slt
-			when others => CONTROLE_ULA <= "---"; -- ??
+		when "00" => CONTROLE_ULA <= "010"; 		-- add para as instruções lw, sw, addi
+		when "01" => CONTROLE_ULA <= "110"; 		-- sub para a instrução beq 
+		when others => case CODIGO_FUNCAO is 			-- instruções do tipo-R
+			when "100000" => CONTROLE_ULA <= "010"; 	-- instrução add
+			when "100010" => CONTROLE_ULA <= "110";	-- instrução sub
+			when "100100" => CONTROLE_ULA <= "000"; 	-- instrução and
+			when "100101" => CONTROLE_ULA <= "001"; 	-- instrução or
+			when "101010" => CONTROLE_ULA <= "111"; 	-- instrução slt
+			when others => CONTROLE_ULA <= "---"; 		-- operação não suportada/inválida
 			end case;
 		end case;
 	end process;
