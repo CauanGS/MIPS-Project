4d51134f7bb93ffadfd0d14c6a77a52485cf068b
CauanGS <cauan.souza@unesp.br>
2025-07-04 21:17:35 -0300

    Pinagem RD1, RD2, Imm, e Resultado feitas. Display de 7 segmentos completo

--- simulation/qsim/work/processador_mips_vlg_check_tst/_primary.vhd
+++ simulation/qsim/work/processador_mips_vlg_check_tst/_primary.vhd
@@ -6,12 +6,21 @@ entity processador_mips_vlg_check_tst is
         BranchSinal     : in     vl_logic;
         ENTRADA_ULA1    : in     vl_logic_vector(31 downto 0);
         ENTRADA_ULA2    : in     vl_logic_vector(31 downto 0);
+        HEX0            : in     vl_logic_vector(6 downto 0);
+        HEX1            : in     vl_logic_vector(6 downto 0);
+        HEX2            : in     vl_logic_vector(6 downto 0);
+        HEX3            : in     vl_logic_vector(6 downto 0);
+        HEX4            : in     vl_logic_vector(6 downto 0);
+        HEX5            : in     vl_logic_vector(6 downto 0);
+        HEX6            : in     vl_logic_vector(6 downto 0);
+        HEX7            : in     vl_logic_vector(6 downto 0);
         INSTR           : in     vl_logic_vector(31 downto 0);
         JumpSinal       : in     vl_logic;
-        PC              : in     vl_logic_vector(31 downto 0);
-        RD1             : in     vl_logic_vector(31 downto 0);
-        RD2             : in     vl_logic_vector(31 downto 0);
-        RESULTADO       : in     vl_logic_vector(31 downto 0);
+        MemToReg        : in     vl_logic;
+        MemWrite        : in     vl_logic;
+        RegDest         : in     vl_logic;
+        RegWrite        : in     vl_logic;
+        ULAsrc          : in     vl_logic;
         sampler_rx      : in     vl_logic
     );
 end processador_mips_vlg_check_tst;
