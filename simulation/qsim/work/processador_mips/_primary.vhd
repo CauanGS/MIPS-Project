d25ade11e33bb1df0aeaef682aa3386b7c7e05b1
Gabriel Sanches Pinto <sanches.pinto@unesp.br>
2025-07-05 17:56:31 -0300

    Memória de dados comentada

--- simulation/qsim/work/processador_mips/_primary.vhd
+++ simulation/qsim/work/processador_mips/_primary.vhd
@@ -2,8 +2,19 @@ library verilog;
 use verilog.vl_types.all;
 entity processador_mips is
     port(
+        BranchResult    : out    vl_logic;
+        CLK_MANUAL      : in     vl_logic;
+        RESET           : in     vl_logic;
+        BranchSinal     : out    vl_logic;
+        JumpSinal       : out    vl_logic;
+        MemToReg        : out    vl_logic;
+        MemWrite        : out    vl_logic;
+        ULAsrc          : out    vl_logic;
+        RegDest         : out    vl_logic;
+        RegWrite        : out    vl_logic;
+        ENTRADA_ULA1    : out    vl_logic_vector(31 downto 0);
+        ENTRADA_ULA2    : out    vl_logic_vector(31 downto 0);
         HEX0            : out    vl_logic_vector(6 downto 0);
-        CLK             : in     vl_logic;
         HEX1            : out    vl_logic_vector(6 downto 0);
         HEX2            : out    vl_logic_vector(6 downto 0);
         HEX3            : out    vl_logic_vector(6 downto 0);
@@ -11,7 +22,6 @@ entity processador_mips is
         HEX5            : out    vl_logic_vector(6 downto 0);
         HEX6            : out    vl_logic_vector(6 downto 0);
         HEX7            : out    vl_logic_vector(6 downto 0);
-        PC              : out    vl_logic_vector(31 downto 0);
-        RESULTADO       : out    vl_logic_vector(31 downto 0)
+        INSTR           : out    vl_logic_vector(31 downto 0)
     );
 end processador_mips;
--- src/MEMORIA_DE_DADOS.vhd
+++ src/MEMORIA_DE_DADOS.vhd
@@ -15,19 +15,19 @@ end MEMORIA_DE_DADOS;
 
 architecture behave of MEMORIA_DE_DADOS is
     type TIPORAM is array(127 downto 0) of STD_LOGIC_VECTOR(31 downto 0); -- cria um um vetor com 128 endereços de 32 bits para simular a memória de dados
-    signal MEMORIA: TIPORAM := (others => (others => '0')); -- Inicializa com zeros
+    signal MEMORIA: TIPORAM := (others => (others => '0')); -- Inicializa tudo com zeros
 begin
     process(CLK)
     begin
-        if rising_edge(CLK) then
-            if RESET = '1' then
-                MEMORIA <= (others => (others => '0'));
+        if rising_edge(CLK) then -- escrita síncrona 
+            if RESET = '1' then 
+                MEMORIA <= (others => (others => '0')); -- se reset ativo mais clock reseta a memória 
             elsif WE = '1' then
-                MEMORIA(to_integer(unsigned(A(7 downto 2)))) <= WD;
+                MEMORIA(to_integer(unsigned(A(7 downto 2)))) <= WD; -- escreve na memoria no endereço 'A'
             end if;
         end if;
     end process;
 
-    -- Leitura fora do clock (combinacional)
+    -- Leitura assincrona, combinacional 
     RD <= MEMORIA(to_integer(unsigned(A(7 downto 2))));
 end behave;
